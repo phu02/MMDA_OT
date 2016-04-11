a05153ff97ea1687ed71e1ceb238577d9f008b54
Jerry Hu <pengxiang.hu@tuft.edu>
2016-04-10 22:08:52 -0400

    Created Kalman Filter iOS project, vDSP playground, and linked mOTivator project

---
+++ Kalman Filter/Kalman_filter iOS/Kalman_filter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kalman_filter
+//
+//  Created by mac-p on 4/10/16.
+//  Copyright © 2016 Tufts University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
