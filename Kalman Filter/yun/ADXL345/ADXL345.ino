4c49af5a20e9bbd1e0112a45583b8a8db14d8a76
Maretta Morovitz <maretta.morovitz@gmail.com>
2016-05-09 14:50:16 -0400

    fixed ardunio sensor files

--- Kalman Filter/yun/ADXL345/ADXL345.ino
+++ Kalman Filter/yun/ADXL345/ADXL345.ino
@@ -1,4 +1,4 @@
-#include <FileIO.h>
+nclude <FileIO.h>
 #include <FreeSixIMU.h>
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
@@ -20,51 +20,50 @@ FreeSixIMU my3IMU = FreeSixIMU();
 
 
 void setup() {
- Serial.begin(9600);
- Wire.begin();
+  Bridge.begin();
+   Serial.begin(9600);
+    Wire.begin();
+     FileSystem.begin();
 
- delay(5);
- my3IMU.init();
- delay(5);
-}
+      delay(5);
+       my3IMU.init();
+        delay(5);
+	}
 
-void loop() {
+	void loop() {
 
-  //Serial.println("test");
+	  String accelString;
+	    String gyroString;
+	      float accel[3];
+	        float gyros[3];
+		  
+		    my3IMU.acc.get_Gxyz(accel);
+		      my3IMU.getAngles(gyros);
+		      //  accel[2] = accel[2]*1.1;
+		        accelString += String(accel[0]);
+			  accelString += ", ";
+			    accelString += String(accel[1]);
+			      accelString += ", ";
+			        accelString += String(accel[2]);
+				  accelString += ", ";  
 
-  String accelString;
-  String gyroString;
-  float accel[3];
-  //float gyros[3];
-  //int *gyro1, *gyro2, *gyro3;
-  float gyros[3];
-  
-  my3IMU.acc.get_Gxyz(accel);
-  my3IMU.getAngles(gyros);
-  accel[2] = accel[2]*1.1;
-  accelString += String(accel[0]);
-  accelString += ", ";
-  accelString += String(accel[1]);
-  accelString += ", ";
-  accelString += String(accel[2]);
-  accelString += ", ";  
-
-  gyroString += String(gyros[0]);
-  gyroString += ", ";
-  gyroString += String(gyros[1]);
-  gyroString += ", ";
-  gyroString += String(gyros[2]);
-  gyroString += ", "; 
+				    gyroString += String(gyros[0]);
+				      gyroString += ", ";
+				        gyroString += String(gyros[1]);
+					  gyroString += ", ";
+					    gyroString += String(gyros[2]);
+					      gyroString += ", "; 
 
 
-  File accelFile = FileSystem.open("/mnt/sd/accel.txt", FILE_WRITE);
-  File gyroFile = FileSystem.open("/mnt/sd/gyro.txt", FILE_WRITE);
+					      //write data to two files
+					      File accelFile = FileSystem.open("/mnt/sd/accel.txt", FILE_APPEND);
+					      File gyroFile = FileSystem.open("/mnt/sd/gyro.txt", FILE_APPEND);
+					          accelFile.println(accelString);
+						      Serial.println(accelString);
+						          accelFile.close();
+							      gyroFile.println(gyroString);
+							          Serial.println(gyroString);
+								      gyroFile.close();
 
-
-  accelFile.println(accelString);
-  gyroFile.println(gyroString);
-
-  Serial.println(accelFile);
-  Serial.println(gyroFile);
-  delay(10);
-}
+								        delay(10);
+									}
